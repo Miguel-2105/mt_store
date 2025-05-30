c16729e59ad4c1cc438e769c33a857301132b589
Miguel Inga <miguel.inga.02@gmail.com>
2025-05-29 23:25:22 -0500

    feat: implement user service, dto and entity

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put } from '@nestjs/common';
+import { ParseIntPipe } from 'src/common/parse-int/parse-int.pipe';
+import { CreateUserDto, UpdateUserDto } from 'src/dto/users.dto';
+import { UsersService } from 'src/services/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
   @Get()
   getUsers() {
-    return 'lista de users';
+    return this.userService.finAll();
   }
 
   @Get(':userId')
-  getUser(@Param('userId') userId: number) {
-    return `el id del user es ${userId}`;
+  getUser(@Param('userId', ParseIntPipe) userId: number) {
+    return this.userService.findOne(userId);
+  }
+
+  @Post()
+  createUser(@Body() payload: CreateUserDto) {
+    return this.userService.create(payload);
+  }
+
+  @Put(':userId')
+  updateUser(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Body() payload: UpdateUserDto,
+  ) {
+    return this.userService.update(userId, payload);
   }
 }
